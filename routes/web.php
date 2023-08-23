7a3b1bf4e33566a4a1ae4ee7b6c8ea663b1ba253
riyad671 <yousufh291@gmail.com>
2023-08-23 08:54:13 +0600

    Add route for admin login

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\AdminController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,4 +15,8 @@ use App\Http\Controllers\HomeController;
 |
 */
 
+// Backend
+ROute::get('/admin',[AdminController::class,'index']);
+
+// Frontend
 Route::get('/',[HomeController::class,'index']);
